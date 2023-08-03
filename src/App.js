3dccd699b50a978d20ea5c977685b0a3000c767e
David Verghese <dj.verghese@gmail.com>
2023-08-03 12:26:27 -0400

    add astronaut and nebula

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import SpinningGlobe from './components/SpinnningGlobe';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import { useLoader } from '@react-three/fiber';
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
 import earth from './textures/earth.jpeg';
 import moon from './textures/moon.jpeg';
-import { OrbitControls } from '@react-three/drei';
-import React, { useRef } from 'react';
+import { OrbitControls, Stars, Cloud } from '@react-three/drei';
+import React from 'react';
 import Galaxy from './components/Galaxy';
+import SpinningGlobe from './components/SpinnningGlobe';
+import sun from './textures/sun.jpeg';
 function App() {
 
-  // how to load texture: https://docs.pmnd.rs/react-three-fiber/tutorials/loading-textures
-  const earthTexture = useLoader(TextureLoader, earth);
-  const moonTexture = useLoader(TextureLoader, moon);
+  
+
   return (
     <div className="App">
       <h1>Welcome to Outer Space!</h1>
@@ -23,7 +23,11 @@ function App() {
         <ambientLight intensity={1} />
         <pointLight position={[10, 10, 10]} />
 
-        <Galaxy/>
+        <Galaxy />
+        <Stars />
+        <Stars position={[0,1,0]} />
+        <Cloud  position={[0, 0, -20]} />
+        <Cloud  position={[0, 1, -20]} />
         {/* orbit controls: https://sbcode.net/react-three-fiber/orbit-controls/ */}
         <OrbitControls />
       </Canvas>
--- src/components/Galaxy.jsx
+++ src/components/Galaxy.jsx
@@ -6,12 +6,24 @@ import moon from '../textures/moon.jpeg';
 import { useLoader } from '@react-three/fiber';
 import { TextureLoader } from 'three/src/loaders/TextureLoader';
 import SpinningGlobe from './SpinnningGlobe';
+import { useGLTF } from '@react-three/drei'
+
 
 const Galaxy = () => {
   const groupRef = useRef();
+
+    // how to load texture: https://docs.pmnd.rs/react-three-fiber/tutorials/loading-textures
+  
   const earthTexture = useLoader(TextureLoader, earth);
   const moonTexture = useLoader(TextureLoader, moon);
 
+  // loading 3d shape from online: https://docs.pmnd.rs/react-three-fiber/tutorials/loading-models
+  
+  function Astronaut() {
+    const gltf = useGLTF('https://thinkuldeep.com/modelviewer/Astronaut.glb')
+   return (<primitive object={gltf.scene} />)
+  }
+  
   useFrame((state, delta) => {
     // Rotate the group with the two shapes around the Y axis
     groupRef.current.rotation.y += 0.01;
@@ -20,7 +32,8 @@ const Galaxy = () => {
   return (
     <group ref={groupRef}>
        <SpinningGlobe toRotate={false} position={[4, 0, -5]} texture={earthTexture} />
-       <SpinningGlobe toRotate={true} position={[-3, 0, -5]} texture={moonTexture} />
+      <SpinningGlobe toRotate={true} position={[-3, 0, -5]} texture={moonTexture} />
+      <Astronaut />
     </group>
   );
 };
